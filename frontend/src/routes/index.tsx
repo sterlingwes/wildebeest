fa2ef15b2e06100f7ea4f27a971a9d56ffe8c992
Wes Johnson <wes@swj.io>
2023-03-05 15:50:41 -0500

    replace main path redirect

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -1,11 +1,35 @@
 import { component$ } from '@builder.io/qwik'
-import { loader$ } from '@builder.io/qwik-city'
 
-export const loader = loader$(({ redirect }) => {
-	redirect(303, '/explore')
-})
+import './index.scss'
 
 export default component$(() => {
-	loader()
-	return <></>
+	// const startStyle = `--start: ${Math.floor(Math.random() * 60)};`
+	return (
+		<div class="overlays" style="--start: 28">
+			<div class="overlay" id="overlay_floor">
+				<div id="overlay_floor-color"></div>
+				<div id="overlay_floor-grid"></div>
+				<div id="overlay_floor-horizon"></div>
+				<div id="overlay_floor-horizon2"></div>
+			</div>
+			<div class="overlay overlay_mountain" id="overlay_mountain">
+				<div class="overlay_mountain-mountain" id="overlay_mountain-mountain"></div>
+			</div>
+			<div class="overlay" id="overlay_sky"></div>
+			<div class="overlay overlay_sun" id="overlay_sun">
+				<div class="overlay_sun-sun" id="overlay_sun-sun"></div>
+			</div>
+			<div class="overlay overlay_sun" id="overlay_sun2">
+				<div class="overlay_sun-sun" id="overlay_sun2-sun"></div>
+			</div>
+			{/* <div class="overlay overlay_sun" id="overlay_sun3">
+				<div class="overlay_sun-sun" id="overlay_sun3-sun"></div>
+			</div> */}
+			<div class="overlay" id="overlay_overlay"></div>
+			<div class="title">
+				<div>dingus</div>
+				<div>business</div>
+			</div>
+		</div>
+	)
 })
